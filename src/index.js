487f1b3c5fd65993742ff06d5bcd328b95b9b94e
JorellLangmar <jorell.langmar@gmail.com>
2020-10-14 15:57:55 +0200

    init aos

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import UserProvider from "./components/Auth/UserProvider";
 import App from "./App";
 
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>;
+<script>
+  AOS.init();
+</script>
+
 import "./styles/reset.css";
 import "./styles/global.css";
 
